f42db0abf156bc8bf8eedddf4ffb03de229c23f2
ChidiRNweke <chidi125@gmail.com>
2024-11-11 12:11:02 +0100

    Make the logo look better on mobile

--- frontend/src/components/BrandLogo.vue
+++ frontend/src/components/BrandLogo.vue
@@ -4,9 +4,11 @@
   >
     <img
       src="../assets/images/logo-removebg.png"
-      class="lg:w-16 w-8 bg-surface-50 rounded-full bg-opacity-70"
+      class="lg:w-16 md:w-8 w-6 bg-surface-50 rounded-full bg-opacity-70"
     />
-    <p class="place-self-center lg:text-lg text-md font-bold pr-4">
+    <p
+      class="place-self-center lg:text-lg md:text-md text-xs font-bold md:pr-4"
+    >
       Who's my Santa
     </p>
   </div>
