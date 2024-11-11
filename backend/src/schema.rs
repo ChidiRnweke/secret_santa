0d26ef01924904c12378079cb73ebe7c51a910f7
ChidiRNweke <chidi125@gmail.com>
2024-11-11 21:11:16 +0100

    Let schema be autogenerated from sql migrations

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,15 +1,29 @@
-diesel::table! {
-    user_assignments (id) {
-        id -> Int4,
-        name -> VarChar,
-        buys_for -> VarChar,
-        session_id -> Int4,
-    }
-}
+// @generated automatically by Diesel CLI.
 
 diesel::table! {
     session (id) {
         id -> Int4,
         created_at -> Timestamp,
+        #[max_length = 255]
+        name -> Varchar,
     }
 }
+
+diesel::table! {
+    user_assignments (id) {
+        id -> Int4,
+        #[max_length = 255]
+        buys_for -> Varchar,
+        #[max_length = 255]
+        buys_from -> Varchar,
+        created_at -> Timestamp,
+        session_id -> Int4,
+    }
+}
+
+diesel::joinable!(user_assignments -> session (session_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    session,
+    user_assignments,
+);
