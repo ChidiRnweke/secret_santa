a81473dbe20b2849b0655317d8233ec09bc35cd4
ChidiRNweke <chidi125@gmail.com>
2024-11-25 22:43:58 +0100

    Refactor assignment logic to use session name instead of ID in SantaSessionModel

--- backend/src/secret_santa/controllers.py
+++ backend/src/secret_santa/controllers.py
@@ -15,16 +15,16 @@ class AssignmentController:
     async def assign(self, users: AssignmentInput) -> AssignmentOutput:
         assignments = users.assign()
         id = self.name_generator.generate_name()
-        while await self.santa_session_repository.check_id_exists(id):
+        while await self.santa_session_repository.check_if_name_exits(id):
             id = self.name_generator.generate_name()
 
         assignment_models = [
             UserAssignmentModel.from_domain_model(assignment)
             for assignment in assignments
         ]
-        santa_session = SantaSessionModel(id=id, name=id, assignments=assignment_models)
+        santa_session = SantaSessionModel(name=id, assignments=assignment_models)
         await self.santa_session_repository.add_santa_session(santa_session)
-        return AssignmentOutput(assignment_id=id, assignments=assignments)
+        return AssignmentOutput(assignment_name=id, assignments=assignments)
 
     async def get_assignment(self, session_id: str, buys_for: str) -> str | None:
         assignment = await self.santa_session_repository.get_assignment(
