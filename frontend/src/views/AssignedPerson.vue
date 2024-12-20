1ffe4e6be596d457a5f7ad9f0a9f6f7edea3eed4
ChidiRNweke <chidi125@gmail.com>
2024-12-20 15:36:19 +0100

    Refactor AssignedPerson.vue to simplify error handling and improve link opened times display

--- frontend/src/views/AssignedPerson.vue
+++ frontend/src/views/AssignedPerson.vue
@@ -2,7 +2,6 @@
 import { useRoute } from 'vue-router'
 import { santaService } from '@/lib/api'
 import { onMounted, ref } from 'vue'
-import ErrorComponent from '@/components/ErrorComponent.vue'
 import Skeleton from 'primevue/skeleton'
 
 const loading = ref(true)
@@ -18,10 +17,8 @@ const senderId = route.params.senderName as string
 
 onMounted(async () => {
   try {
-    const { gift_sender, gift_receiver, times_viewed } = await santaService.getAssignment(
-      santaId,
-      senderId,
-    )
+    const { gift_sender, gift_receiver, times_viewed } =
+      await santaService.getAssignment(santaId, senderId)
     sender.value = gift_sender
     receiver.value = gift_receiver
     linkOpenedTimes.value = times_viewed
@@ -36,11 +33,6 @@ onMounted(async () => {
 </script>
 
 <template>
-  <ErrorComponent
-    v-model:visible="error"
-    :errorMessage="errorMessage"
-    @update:visible="error = $event"
-  />
   <section
     v-if="loading"
     class="text-center grid grid-cols-1 gap-y-8 justify-items-center border-box lg:mx-32 md:mx-20 mx-8 mb-32"
@@ -52,7 +44,7 @@ onMounted(async () => {
   <section v-if="error" class="text-center">
     <h1 class="text-6xl mb-20">An error occurred.</h1>
     <p class="text-xl">
-      We're sorry, but we couldn't find your Secret Santa assignment. Please try
+      We're sorry, but we couldn't find who you need get a gift for. Please try
       again later.
     </p>
   </section>
@@ -71,9 +63,13 @@ onMounted(async () => {
       You can now start preparing your gift for {{ receiver }}. Good luck and
       have fun! 🎁
     </p>
-    <p v-if="linkOpenedTimes==1">
-      ✨ You're the first to open this link. ✨
+    <p v-if="linkOpenedTimes == 1">✨ You're the first to open this link. ✨</p>
+    <p v-else>
+      PS: this link has been opened
+      <span class="text-primary-700 dark:text-primary-300">{{
+        linkOpenedTimes
+      }}</span>
+      times.
     </p>
-    <p v-else>PS: this link has been opened <span class="text-primary-700 dark:text-primary-300">{{ linkOpenedTimes }}</span> times.</p>
   </section>
 </template>
